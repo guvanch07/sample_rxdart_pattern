499124bf7cbe009e84c5c48e829411ab3bc181d3
guvanch07 <guvanchamanov777@gmail.com>
2022-11-29 15:04:48 +0300

    done

--- lib/data/core/error_handler.dart
+++ lib/data/core/error_handler.dart
@@ -5,12 +5,6 @@ class ServerException implements Exception {
 
 class CacheException implements Exception {}
 
-class ConfirmCodeException implements Exception {}
-
-class CreateUserException implements Exception {}
-
-class GetUserProfileException implements Exception {}
-
 class ToManyRequestsException implements Exception {}
 
 class BadRequestException implements Exception {
@@ -22,24 +16,6 @@ class NotFoundException implements Exception {
   dynamic message;
   NotFoundException([this.message]);
 }
-
-class PasswordMismatchException implements Exception {
-  /// account_password_mismatch
-}
-
-class AccountNotFoundException implements Exception {
-  /// account_not_found server response
-}
-
-class WrongEmailException implements Exception {
-  /// must_be_valid_email_address server response
-}
-
-class EmailAlreadyBeenTakenException implements Exception {
-  /// has_already_been_taken
-}
-
-class CreateOwnProjectException implements Exception {}
 
 class FetchVocabulariesException implements Exception {}
 
